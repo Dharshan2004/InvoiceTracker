e88d5dabef7eddde442e38d003587e1b86b9058d
Kannan Priyadharshan <kpd2204@gmail.com>
2024-09-12 18:26:23 +0800

    [IVT-0] Created base templates for authentication and index

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from app import create_app
 
 if __name__ == "__main__":
     app = create_app()
-    app.run()
+    app.run(host="0.0.0.0", port=8001, debug=True)
--- app/main/views.py
+++ app/main/views.py
@@ -4,3 +4,11 @@ from . import main
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/login')
+def login():
+    return render_template('login.html')
+
+@main.route('/signup')
+def signup():
+    return render_template('signup.html')
