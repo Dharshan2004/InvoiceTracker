64c6bf29250174e49cf257803be16840b24882d6
Kannan Priyadharshan <kpd2204@gmail.com>
2024-09-16 00:09:02 +0800

    [IVT-0] Document Generation Base Functions created

---
+++ app/document_gen.py
@@ -0,0 +1,58 @@
+from docx import Document
+
+def replace_text_in_runs(paragraph, old_text, new_text):
+    # Iterate over each run in the paragraph
+    for run in paragraph.runs:
+        if old_text in run.text:
+            # Replace text in the run while preserving formatting
+            run.text = run.text.replace(old_text, new_text)
+
+def add_table_entires(table, entries):
+    for entry in entries:
+        row = table.add_row()  # Add a new row
+        for i, cell_text in enumerate(entry):
+            row.cells[i].text = cell_text
+
+def replace_text_in_docx(input_file, output_file, entries, table_entries):
+    # Load the document
+    doc = Document(input_file)
+    
+    # Loop through each paragraph in the document
+    for entry in entries.keys():
+        old_text = entry
+        new_text = entries[entry]
+        for paragraph in doc.paragraphs:
+            replace_text_in_runs(paragraph, old_text, new_text)
+    
+    table = doc.tables[0]
+    add_table_entires(table, table_entries)
+
+    # Save the modified document as a new file
+    doc.save(output_file)
+
+
+
+
+# Test the function
+input_file = 'template_quotation.docx'
+output_file = 'new_document.docx'
+entries = {
+    "[name]": "John Doe",
+    "[company]": "ABC Inc.",
+    "[date]": "01/01/2022",
+    "[address]": "123 Main St",
+    "[contact]": "123-456-7890",
+    "[ref_no]": "Q12345",
+    "[so_no]": "NIL",
+    "[terms]": "30 days",
+    "[LEW Name]": "Jane Smith",
+    "[LEW No]": "12345",
+    "[LEW Email]": "hello@hello.com",
+    "[LEW Contact]": "987-654-3210",
+}
+table_entiries = [["1", "LEW Services", "1", "1000", "1000"]]
+
+# Not used in production
+if __name__ == '__main__':
+    replace_text_in_docx(input_file, output_file, entries, table_entiries)
+
--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,18 @@
-from flask import render_template, session, redirect, url_for
+from flask import render_template, session, redirect, url_for, request
 from . import main
 
 @main.route('/')
 def index():
     return render_template('index.html')
 
-@main.route('/login')
+@main.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        pass
     return render_template('login.html')
 
-@main.route('/signup')
+@main.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        pass
     return render_template('signup.html')
